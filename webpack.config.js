9a2bb1e40c3e82c8917ee33f11ec9f4a815e9cbf
Clement Liscoet <clement.liscoet@gmail.com>
2021-05-07 12:00:04 +0100

    config: babel set up

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,13 @@ const webpack = require("webpack");
 module.exports = {
   entry: "./src/client/index.js",
   mode: "development",
-};
-
-// output: { ...output options }
-module: {
   rules: [
     {
       test: "/.js$/",
       exclude: /node_modules/,
       loader: "babel-loader",
     },
-  ];
-}
+  ],
+};
+
+// output: { ...output options }
