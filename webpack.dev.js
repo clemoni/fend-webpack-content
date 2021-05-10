85556e50d5c3248021716e89fb50abd67968e50f
Clement Liscoet <clement.liscoet@gmail.com>
2021-05-10 13:38:41 +0100

    config: add webpack server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,17 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "development",
-  devtool: "source-map",
+  devtool: "inline-source-map",
+  // devServer: {
+  //   contentBase: "./dist",
+  // },
   module: {
     rules: [
       {
@@ -24,6 +30,16 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new BundleAnalyzerPlugin(),
   ],
 };
 
